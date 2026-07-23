cad9bb54fdf70e66950142581ba71b5d85b3b86f
Jhônata Ruan <jrsvalentin@gmail.com>
2026-07-23 15:26:22 -0300

    Iniciando projeto remessa + dependencias

---
+++ src/main/java/com/inter/java/challenge/InterJavaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.inter.java.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterJavaChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterJavaChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inter/java/challenge/InterJavaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inter.java.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterJavaChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
